96b036250cfe2ccaddc71312cbcdc142f2fb8523
Lorenzo <cap.lorenzo@yahoo.it>
2023-04-06 20:58:45 +0200

    minor change to Antipasti

--- src/Antipasti.java
+++ src/Antipasti.java
@@ -2,61 +2,28 @@ import java.util.*;
 
 public class Antipasti {
     private String nome;
-    private String ingredienti;
     private Double prezzo;
-    private static List<String> NomeAntipasti = new ArrayList<>(Arrays.asList("Affettato", "Vitello Tonnato", "Olive Ascolane", "Cornetti Salati"));
-    private static List<String> IngredientiAntipasti = new ArrayList<>(Arrays.asList("Carne mista", "Carne bovina, uova", "Pane, carne bovina, formaggio", "Burro, Carne mista, Semi di Sesamo"));
-    private static List<Double> PrezzoAntipasti = new ArrayList<>(Arrays.asList(7.50, 8.50, 6.50, 5.50));
-
-    private void setAntipasti(String nome, String ingredienti, Double prezzo) {
+    private static HashMap<String, Double> listaAntipasti = new LinkedHashMap<>();
+    private void initListAntipasti(String nome, Double prezzo) {
         this.nome = nome;
-        this.ingredienti = ingredienti;
         this.prezzo = prezzo;
+        listaAntipasti.put(nome, prezzo);
     }
-    public void Antipasti() {
-        System.out.println("Antipasti di carne:" +
-                "\n");
-        for (int i = 0; i < NomeAntipasti.size();i++) {
-            setAntipasti(NomeAntipasti.get(i), IngredientiAntipasti.get(i), PrezzoAntipasti.get(i));
-            System.out.println(getNome() +
-                    "\n         Ingredienti : " + getIngredienti() +
-                    "\n         Prezzo : " + getPrezzo() + " €");
+    public void printAntipasti() {
+        initListAntipasti("Affettato", 7.50);
+        initListAntipasti("Vitello Tonnato", 8.50);
+        initListAntipasti("Olive Ascolane", 6.50);
+        initListAntipasti("Cornetti Salati", 5.50);
+        System.out.println("Antipasti: \n");
+        for (Map.Entry<String, Double> Antipasti : listaAntipasti.entrySet()) {
+            System.out.println(Antipasti.getKey() + " - " + Antipasti.getValue() + "€\n");
         }
     }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getIngredienti() {
-        return ingredienti;
-    }
-
-    public void setIngredienti(String ingredienti) {
-        this.ingredienti = ingredienti;
     }
 
     public Double getPrezzo() {
         return prezzo;
     }
-
-    public void setPrezzo(Double prezzo) {
-        this.prezzo = prezzo;
-    }
-
-//    public void PrintMenu(){}
-
-//    public static void StampaMenuAntipasti () {
-//        FormatMenu(NomeAnitpasti, IngredientiAnitpasti, PrezzoAntipasti);
-//    }
-
-//    public void newDish (String Nome, String Ingredienti, Double Prezzo) {
-//        NomeAnitpasti.add(Nome);
-//        IngredientiAnitpasti.add(Ingredienti);
-//        PrezzoAntipasti.add(Prezzo);
-//    }
 }
