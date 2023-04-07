93bfa67d652af72b89142f996523b53346e4a3e6
michelepc <mangiacotti.fi@gmail.com>
2023-04-07 14:52:15 +0200

    code review 07/04

--- src/Antipasti.java
+++ src/Antipasti.java
@@ -3,27 +3,30 @@ import java.util.*;
 public class Antipasti {
     private String nome;
     private Double prezzo;
-    private static HashMap<String, Double> listaAntipasti = new LinkedHashMap<>();
-    private void initListAntipasti(String nome, Double prezzo) {
+
+    Antipasti(String nome, Double prezzo) {
         this.nome = nome;
         this.prezzo = prezzo;
-        listaAntipasti.put(nome, prezzo);
     }
-    public void printAntipasti() {
-        initListAntipasti("Affettato", 7.50);
-        initListAntipasti("Vitello Tonnato", 8.50);
-        initListAntipasti("Olive Ascolane", 6.50);
-        initListAntipasti("Cornetti Salati", 5.50);
-        System.out.println("Antipasti: \n");
-        for (Map.Entry<String, Double> Antipasti : listaAntipasti.entrySet()) {
-            System.out.println(Antipasti.getKey() + " - " + Antipasti.getValue() + "€\n");
-        }
-    }
+
     public String getNome() {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public Double getPrezzo() {
         return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void print(){
+        String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+        System.out.println(ANSI_YELLOW_BACKGROUND + nome + prezzo +ANSI_YELLOW_BACKGROUND);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        Antipasti antipasti = new Antipasti();
-        antipasti.printAntipasti();
-        Primi_Piatti primiPiatti = new Primi_Piatti();
-        primiPiatti.printPrimiPiatti();
-        SecondiPiatti secondiPiatti = new SecondiPiatti();
-        secondiPiatti.printSecondiPiatti();
-        Bevande bevande = new Bevande();
-        bevande.stampaBevande();
-        Dolci dolci = new Dolci();
-        dolci.stampaDolci();
+        /**
+         *     antipasti.printAntipasti();
+         *         Primi_Piatti primiPiatti = new Primi_Piatti();
+         *         primiPiatti.printPrimiPiatti();
+         *         SecondiPiatti secondiPiatti = new SecondiPiatti();
+         *         secondiPiatti.printSecondiPiatti();
+         *         Bevande bevande = new Bevande();
+         *         bevande.stampaBevande();
+         *         Dolci dolci = new Dolci();
+         *         dolci.stampaDolci();
+         */
+
+
+
+        Antipasti antipasti1 = new Antipasti("Affettato", 7.50);
+        Antipasti antipasti2 = new Antipasti("Vitello Tonnato", 8.50);
+        Antipasti antipasti3 = new Antipasti("Olive Ascolane", 6.50);
+        Antipasti antipasti4 = new Antipasti("Cornetti Salati", 5.50);
+
+        antipasti1.print();
+        antipasti2.print();
+        antipasti3.print();
+        antipasti4.print();
+
     }
 }
