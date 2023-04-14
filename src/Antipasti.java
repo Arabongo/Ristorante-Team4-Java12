5390511f672196a3dba54d4e2aa63f6cc8c8db21
Lorenzo <cap.lorenzo@yahoo.it>
2023-04-14 11:19:51 +0200

    Minor changes

--- src/Antipasti.java
+++ src/Antipasti.java
@@ -1,4 +1,23 @@
 public class Antipasti {
+    private String name;
+    private double price;
 
+    Antipasti (String name, Double price) {
+        this.name = name;
+
+        this.price = price;
+
+    }
+    public void setPrice(Double price) {this.price = price;}
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getPrice() {
+        return price;
+    }
 }
 
