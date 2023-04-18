3aac26a92a382d3c20174d7c1141065521e697ca
Lorenzo <cap.lorenzo@yahoo.it>
2023-04-18 15:29:29 +0200

    Added class Piatti

---
+++ src/Piatti.java
@@ -0,0 +1,42 @@
+public class Piatti {
+    private String name;
+    private double price;
+    private String ingredient;
+
+    public Piatti(String name, double price, String ingredient) {
+        this.name = name;
+        this.price = price;
+        this.ingredient = ingredient;
+    }
+
+    public void print () {
+        System.out.println(name + price + ingredient);
+    }
+
+    //Fare un override per le proprie classi per il metodo print
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+}
