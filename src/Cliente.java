474a9a77a93558056ad009f3114a561310012e87
Drew <andreaochi9@hotmail.it>
2023-05-19 11:13:15 +0200

    modifica calsse cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,16 +3,12 @@ import enumerati.TipoMenuEnum;
 public class Cliente {
     private String nome;
     private String cognome;
-    private int numeroDiTelefono;
-    private String eMail;
     private TipoMenuEnum preferenzaMenu;
 
 
-    public Cliente(String nome, String cognome, int numeroDiTelefono, String eMail, TipoMenuEnum preferenzaMenu) {
+    public Cliente(String nome, String cognome, TipoMenuEnum preferenzaMenu) {
         this.nome = nome;
         this.cognome = cognome;
-        this.numeroDiTelefono = numeroDiTelefono;
-        this.eMail = eMail;
         this.preferenzaMenu = preferenzaMenu;
     }
 
@@ -23,31 +19,12 @@ public class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getCognome() {
         return cognome;
     }
-
     public void setCognome(String cognome) {
         this.cognome = cognome;
     }
-
-    public int getNumeroDiTelefono() {
-        return numeroDiTelefono;
-    }
-
-    public void setNumeroDiTelefono(int numeroDiTelefono) {
-        this.numeroDiTelefono = numeroDiTelefono;
-    }
-
-    public String geteMail() {
-        return eMail;
-    }
-
-    public void seteMail(String eMail) {
-        this.eMail = eMail;
-    }
-
     public TipoMenuEnum getPreferenzaMenu() {
         return preferenzaMenu;
     }
