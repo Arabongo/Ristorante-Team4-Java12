6e2769b95b39e421fb735dfdb84263813e80b225
Arabongo <edosurfer118@gmail.com>
2023-05-27 17:19:23 +0200

    udate class bevandeDAO

--- src/main/java/BevandeDAO.java
+++ src/main/java/BevandeDAO.java
@@ -1,4 +1,5 @@
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BevandeDAO {
@@ -11,6 +12,40 @@ public class BevandeDAO {
         statement.setString(3, bevande.getIngredients().toString());
         statement.setBoolean(4, bevande.isAlcoholic());
         statement.setDouble(5, bevande.getAlcoholPercentage());
+
+        statement.executeUpdate();
+        statement.close();
+    }
+    public Bevande retriveBevande(Bevande bevande) throws SQLException {
+        String query = "SELECT NAME, PRICE, INGREDIENTS, ALCOHOLIC, ALCOHOL_PERCENTAGE FROM BEVANDE WHERE NAME = ?";
+        PreparedStatement statement = ConnectionProvider.getInstance().prepareStatement(query);
+        statement.setString(1, bevande.getName());
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            Double price = resultSet.getDouble("PRICE");
+            Boolean alcoholic = resultSet.getBoolean("ALCOHOLIC");
+            Double alcoholPercentage = resultSet.getDouble("ALCOHOL_PERCENTAGE");
+            Bevande selectedDish = new Bevande(bevande.getName(), price, bevande.getIngredients(),alcoholic, alcoholPercentage, false, bevande.getTipoMenuEnum());
+            return selectedDish;
+        }
+        return null;
+    }
+    public void updateBevande (Bevande bevande) throws SQLException {
+        String query = "UPDATE BEVANDE SET PRICE = ?, INGREDIENTS = ?, ALCOHOLIC = ?, ALCOHOL_PERCENTAGE = ? WHERE NAME = ?";
+        PreparedStatement statement = ConnectionProvider.getInstance().prepareStatement(query);
+        statement.setDouble(1, bevande.getPrice());
+        statement.setString(2, bevande.getIngredients().toString());
+        statement.setBoolean(3, bevande.isAlcoholic());
+        statement.setDouble(4, bevande.getAlcoholPercentage());
+        statement.setString(5, bevande.getName());
+
+        statement.executeUpdate();
+        statement.close();
+    }
+    public void deleteBevande(String name) throws SQLException {
+        String query = "DELETE FROM ANTIPASTI WHERE NAME = ?";
+        PreparedStatement statement = ConnectionProvider.getInstance().prepareStatement(query);
+        statement.setString(1, name);
 
         statement.executeUpdate();
         statement.close();
