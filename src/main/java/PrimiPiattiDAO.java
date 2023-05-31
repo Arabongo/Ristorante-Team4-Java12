6fed3988e161c19a1549c4170d5aef49086e2d8c
AlexBlackwolf <alex_fortini@hotmail.com>
2023-05-31 02:30:01 +0200

    modified and edited primipiattiDAO

--- src/main/java/PrimiPiattiDAO.java
+++ src/main/java/PrimiPiattiDAO.java
@@ -10,8 +10,8 @@ public class PrimiPiattiDAO {
     private static final String INGREDIENTI = "INGREDIENTI";
     private static final String DISH_OF_THE_DAY = "DISH_OF_THE_DAY";
 
-    public void createPrimiPiatti (PrimiPiatti primiPiatti) throws SQLException {
-        String quary = "INSERT INTO "+TABLE+"("+PASTA_TYPE+", "+NAME+", "+PRICE+", "+INGREDIENTI+","+DISH_OF_THE_DAY+")" +
+    public void createPrimiPiatti(PrimiPiatti primiPiatti) throws SQLException {
+        String quary = "INSERT INTO " + TABLE + "(" + PASTA_TYPE + ", " + NAME + ", " + PRICE + ", " + INGREDIENTI + "," + DISH_OF_THE_DAY + ")" +
                 "VALUES (?, ?, ?, ?, ?)";
         PreparedStatement statement = ConnectionProvider.getInstance().prepareStatement(quary);
         statement.setString(1, primiPiatti.getPastaType());
@@ -23,8 +23,9 @@ public class PrimiPiattiDAO {
         statement.executeUpdate();
         statement.close();
     }
+
     public PrimiPiatti retrievePrimiPiatti(PrimiPiatti primopiatto) throws SQLException {
-        String query = "SELECT "+PASTA_TYPE+", "+NAME+", "+PRICE+", "+INGREDIENTI+", "+DISH_OF_THE_DAY+" FROM "+TABLE+" WHERE "+NAME+" = ?";
+        String query = "SELECT " + PASTA_TYPE + ", " + NAME + ", " + PRICE + ", " + INGREDIENTI + ", " + DISH_OF_THE_DAY + " FROM " + TABLE + " WHERE " + NAME + " = ?";
         PreparedStatement statement = ConnectionProvider.getInstance().prepareStatement(query);
         statement.setString(1, primopiatto.getName());
         ResultSet resultSet = statement.executeQuery();
@@ -33,31 +34,32 @@ public class PrimiPiattiDAO {
             Double price = resultSet.getDouble(PRICE);
             String pasta_type = resultSet.getString(PASTA_TYPE);
             Boolean canBeDishOfTheDay = resultSet.getBoolean(DISH_OF_THE_DAY);
-            PrimiPiatti selectedDish = new PrimiPiatti(nome,price,primopiatto.getIngredients(),pasta_type,canBeDishOfTheDay,primopiatto.getTipoMenuEnum());
+            PrimiPiatti selectedDish = new PrimiPiatti(nome, price, primopiatto.getIngredients(), pasta_type, canBeDishOfTheDay, primopiatto.getTipoMenuEnum());
             return selectedDish;
         }
         return null;
     }
 
-    /*public void updateAntipasti (Antipasti antipasto) throws SQLException {
-        String query = "UPDATE ANTIPASTI SET PRICE = ?, INGREDIENTI = ?, PORZIONI = ?, LOCAL_PRODUCT = ? WHERE NAME = ?";
+    public void updatePrimiPiatti(PrimiPiatti primopiatto) throws SQLException {
+        String query = "UPDATE PRIMIPIATTI SET " + PRICE + " = ?, " + INGREDIENTI + " = ?, " + PASTA_TYPE + " = ?, " + DISH_OF_THE_DAY + " = ? WHERE " + NAME + " = ?";
         PreparedStatement statement = ConnectionProvider.getInstance().prepareStatement(query);
-        statement.setDouble(1, antipasto.getPrice());
-        statement.setString(2, antipasto.getIngredients().toString());
-        statement.setInt(3, antipasto.getPortions());
-        statement.setBoolean(4, antipasto.getLocalProduct());
-        statement.setString(5, antipasto.getName());
+        statement.setDouble(1, primopiatto.getPrice());
+        statement.setString(2, primopiatto.getIngredients().toString());
+        statement.setString(3, primopiatto.getPastaType());
+        statement.setBoolean(4, primopiatto.isCanBeDishOfTheDay());
+        statement.setString(5, primopiatto.getName());
 
         statement.executeUpdate();
         statement.close();
     }
 
     public void deleteAntipasti(String name) throws SQLException {
-        String query = "DELETE FROM ANTIPASTI WHERE NAME = ?";
+        String query = "DELETE FROM PRIMIPIATTI WHERE " + NAME + " = ?";
         PreparedStatement statement = ConnectionProvider.getInstance().prepareStatement(query);
         statement.setString(1, name);
 
         statement.executeUpdate();
         statement.close();
-    }*/
+
+    }
 }
